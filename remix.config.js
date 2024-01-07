422e3b477d995876db1561ea5666ca12fde321cf
manu-xo5 <mohit.matwaya@gmail.com>
2024-01-08 00:47:30 +0530

    chore: add tailwindcss.ts

--- remix.config.js
+++ remix.config.js
@@ -3,11 +3,4 @@ import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...(process.env.NODE_ENV === "production" ? config : undefined),
-
-  tailwind: true,
-  postcss: true,
-  // This works out of the box with the Netlify adapter, but you can
-  // add your own custom config here if you want to.
-  //
-  // See https://remix.run/file-conventions/remix-config
 };
