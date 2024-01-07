2037b5a66f7f178dd1e72e9905b3249daf1db0f4
manu-xo5 <mohit.matwaya@gmail.com>
2024-01-08 00:53:22 +0530

    chore: add existing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,52 @@
-import type { Config } from 'tailwindcss'
+import plugin from "tailwindcss/plugin";
+import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: (theme) => ({
+        primary: theme.colors.zinc[700],
+        secondary: theme.colors.zinc[800],
+        border: theme.colors.zinc[500],
+        accent: theme.colors.blue[600],
+        "text-primary": "#E5E5E5",
+      }),
+      spacing: {
+        sm: "1.5em 2.5em",
+        md: "2.5em 3.5em",
+        3.75: "0.9375rem",
+        4.5: "1.125rem",
+      },
+      borderRadius: {
+        sm: "5px",
+      },
+      borderColor: (theme) => ({
+        DEFAULT: theme.colors.zinc[500],
+      }),
+      lineHeight: {
+        19: "76px",
+      },
+      width: {
+        6.5: "1.625rem",
+        19: "76px",
+      },
+      fontSize: {
+        title: ["1.125rem", { lineHeight: "1.3em" }],
+        sm: ["0.875rem", { lineHeight: "1.2em" }],
+        base: ["14px", { lineHeight: "1.285em" }],
+        icon: ["44px", "44px"],
+      },
+    },
   },
-  plugins: [],
-} satisfies Config
-
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        html: {
+          color: theme("colors.text-primary"),
+          backgroundColor: theme("colors.secondary"),
+        },
+      });
+    }),
+  ],
+} satisfies Config;
